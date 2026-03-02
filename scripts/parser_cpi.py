3d31c5aa8e18d6602ff2ecc42b8e286ebd9baa4a
Sabina <124915433+Sabinashamurzaeva@users.noreply.github.com>
2026-03-02 06:32:09 +0300

    Парсинг предобработка и виузаилия 3 показателя

--- scripts/parser_cpi.py
+++ scripts/parser_cpi.py
@@ -17,3 +17,18 @@ def download_file():
 def preprocess():
     os.makedirs("data/processed", exist_ok=True)
     df = pd.read_excel(RAW_PATH, skiprows=3)
+    df = df.rename(columns={df.columns[0]: "month"})
+    df = df.dropna(subset=["month"])
+    df = df.melt(id_vars=["month"], var_name="year", value_name="value")
+    df = df.dropna(subset=["value"])
+    df["value"] = df["value"].astype
+    
+    URL = "https://77.rosstat.gov.ru/storage/mediabank/Динамика%20индекса%20потребительских%20цен%20на%20товары%20и%20услуги%20в%202000-2026%20гг..xlsx"
+
+r = requests.get(URL, verify=False)
+r.raise_for_status()
+
+with open("cpi.xlsx", "wb") as f:
+    f.write(r.content)
+
+print("Файл скачан")
