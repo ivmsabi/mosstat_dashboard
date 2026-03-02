23ec315b1863ec0f5417d3e1eb225366c9f3218e
Sabina <124915433+Sabinashamurzaeva@users.noreply.github.com>
2026-03-02 06:07:22 +0300

    Initial commit: Парсер 1 показателя и его предобработка и анализ

---
+++ scripts/parser_cpi.py
@@ -0,0 +1,19 @@
+import os
+import requests
+import pandas as pd
+
+URL = "https://77.rosstat.gov.ru/storage/mediabank/Динамика%20индекса%20потребительских%20цен%20на%20товары%20и%20услуги%20в%202000-2026%20гг..xlsx"
+RAW_PATH = "data/raw/cpi.xlsx"
+PROCESSED_PATH = "data/processed/cpi_cleaned.csv"
+
+def download_file():
+    os.makedirs("data/raw", exist_ok=True)
+    r = requests.get(URL, verify=False)  # отключаем проверку SSL
+    r.raise_for_status()
+    with open(RAW_PATH, "wb") as f:
+        f.write(r.content)
+    print("Файл скачан")
+
+def preprocess():
+    os.makedirs("data/processed", exist_ok=True)
+    df = pd.read_excel(RAW_PATH, skiprows=3)
